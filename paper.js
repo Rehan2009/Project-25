34de0380ec01135ebbd15d162c9da3a32d617bf6
Rehan2009 <77333649+Rehan2009@users.noreply.github.com>
2021-01-29 17:49:40 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,29 @@
+class Paper{
+    constructor(x, y) {
+        var options = {
+            'restitution':0.3,
+            'friction':0.5,
+            'density':1.2
+        }
+        this.body = Bodies.rectangle(x, y,25,25, options);
+        this.width = 20;
+        this.height = 20;
+        this.image = loadImage("paper.png");
+        World.add(world, this.body);
+      }
+      display(){
+        push();
+        
+        imageMode(CENTER);
+        image(this.image,800,350,30,30);   
+        if (keyCode === UP_ARROW) {
+          this.body.position.y = -50
+          this.body.position.x = 1200
+          
+            } 
+        if(this.body.position.y === -50){
+          Matter.Body.setStatic(this.body,false);
+        }
+        pop();
+      }
+}
